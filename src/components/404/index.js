6b42cf531b4e33dfc4a6b333495705502b2cb1b8
Rohman Sheikh <Rohman_S@Blackpool-MacBook.local>
2017-12-01 11:50:47 +0100

    Add Routes

---
+++ src/components/404/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import errorGif from './404.gif';
+
+const Errors = () => (
+    <img src={errorGif} alt='error'/>
+);
+
+export default Errors;
---
+++ src/routes.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import {
+    BrowserRouter as Router,
+    Route,
+    Switch
+} from 'react-router-dom';
+import Home from './components/home';
+import Profile from './components/profile';
+import error404 from './components/404';
+import Galery from './components/gallery';
+import Sign from './components/sign';
+import App from './components/app';
+
+const MyRoute = () => (
+    <Router>
+        <div>
+            <App name='layout'>
+                <Switch>
+                    <Route exact path="/" component={Home} />
+                    <Route path="/profile" component={Profile} />
+                    <Route path="/galery" component={Galery} />
+                    <Route path="/sign" component={Sign} />
+                    <Route component={error404} />
+                </Switch>
+            </App>
+        </div>
+    </Router>
+);
+
+export default MyRoute;
+
